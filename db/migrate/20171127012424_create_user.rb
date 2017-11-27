188db579700b78c1fa0905b386c31a965665079b
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-11-26 18:29:31 -0700

    Create users

---
+++ db/migrate/20171127012424_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
